2452a789e44203d5dfe8cf386b0480475100cba9
bluehawk27 <avillacorta27@gmail.com>
2015-04-07 12:54:47 -0700

    add recipe image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  #Paperclip path
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
