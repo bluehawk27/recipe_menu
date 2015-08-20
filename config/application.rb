15019810bb47a78f18ef5ebf41d483b58b68667d
bluehawk27 <avillacorta27@gmail.com>
2015-08-19 17:17:26 -0700

    .env fix heroku

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module RecipeMenu
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    Dotenv.load(File.expand_path("../../.env.#{Rails.env}", __FILE__))
+    # Dotenv.load(File.expand_path("../../.env.#{Rails.env}", __FILE__))
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
